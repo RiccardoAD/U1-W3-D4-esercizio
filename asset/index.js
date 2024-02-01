0b5a4f176d463bc078502a1a62ad6e22f099ca70
RiccardoAD <rick_95@libero.it>
2024-02-01 18:01:35 +0100

    second.commit

--- asset/index.js
+++ asset/index.js
@@ -5,9 +5,9 @@ const creazioneCelle = function (cell) {
     for (let i = 1; i <=76 ; i++) {
         const divCella=document.createElement("div");  // creo i div cellaa dell tombola
         divCella.className= "cellette";
-        const numeroCelle=document.createElement("h4"); // metto in ogni cella i numeri 
-        numeroCelle.innerText=i;
-        divCella.appendChild(numeroCelle);
+        const numeroCelle=document.createElement("h4"); // creo variabile contenente h4 con i numeri
+        numeroCelle.innerText=i;                     // metto h4 interno alle celle 
+        divCella.appendChild(numeroCelle);    
         cellaContenuto.appendChild(divCella);
 
 
@@ -16,16 +16,20 @@ const creazioneCelle = function (cell) {
     
 }
 
-const button=document.getElementById("button")  
+const button=document.querySelector("#button") 
 
-button.onclick=function () {
+
+
+button.onclick = ()=> {
     
     
-    const celle=document.querySelectorAll("cellette") 
-    const num=Math.floor(Math.random()*76)
-    const celleRandom=celle[num]
-    console.log(celleRandom);
-    celleRandom.style.backgroundColor= "green"
+    const numRandom=Math.floor(Math.random()*76) //genero un variabile che contiene il numero random
+    const arrayCelle=document.querySelectorAll(".cellette") // creo un erray delee cellette della tombola 
+     const numEstratto=arrayCelle[numRandom-1]   // nem estratto è il nodo elemento ,-1 perchè il con delerray parte da 1  e li confronto
+     console.log(numRandom, arrayCelle[numRandom-1]);
+     numEstratto.style="background-color: green"; // ora che ho il nodo posso modificare lo style 
+    
+    
     
     
 }
